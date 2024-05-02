5dd5b203d394a9f3b40530309d654763755789b0
thatsOven <68780017+thatsOven@users.noreply.github.com>
2024-05-02 22:55:00 +0200

    the version again :)

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from typing      import Callable, ClassVar, NoReturn
 
 from modules.sockets import *
 
-VERSION = "2024.5.1-ALPHA"
+VERSION = "2024.5.2-ALPHA"
 SETTINGS_VERSION = "1"
 
 MAIN_RESOLUTION     = "380x305"
